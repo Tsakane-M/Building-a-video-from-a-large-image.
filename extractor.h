a929be82fe50280b667579820182b9a783150732
peter <peetmhlanga@gmail.com>
2021-03-30 23:44:24 +0200

    created Frames class

--- extractor.h
+++ extractor.h
@@ -1,6 +1,38 @@
-#include<iostream>
+#ifndef _EXTRACTOR_H
+#define _EXTRACTOR_H
 
-int main(void){
+//includes
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
 
-    return 0;
+namespace MHLPET015{
+
+
+// A CLASS DECLARATION,
+class FrameSequence{
+    
+    std::vector<unsigned char **> imageSequence;// stores each extracted frame image
+                                                // as a dynamically allocated unsigned
+                                                // char array,
+
+    public:
+        FrameSequence(void){
+
+        }
+        ~FrameSequence{
+
+        }
+
+        setFrameSize(int width,int height){
+
+        }
+};
+
+
+
+
 }
+#endif // matches the #ifndef
